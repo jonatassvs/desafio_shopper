34d76eca3887b7cefe10dfd50841529c24a8e2d0
Jonatas <jonatas_s.v.s@hotmail.com>
2024-08-29 15:31:44 -0300

    configuracoes iniciais

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', function (req: Request, res: Response) {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
