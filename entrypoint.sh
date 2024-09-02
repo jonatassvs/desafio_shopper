66ba89c6eae77f1e1f707ada525536a88b057936
Jonatas Vieira <jonatas_s.v.s@hotmail.com>
2024-09-01 23:41:26 -0300

    finalizacao confg docker (#4)

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Rodar o build
+npm run build
+
+# Iniciar o aplicativo
+npm run start
--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 // Inicializa o Sequelize com o SQLite
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite',  // Caminho para o arquivo do banco de dados SQLite
+  storage: './database.sqlite',
 });
 
 export default sequelize;
